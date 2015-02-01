3ab28a738a1cbe15cc832a394ec08d3ede81c0ba
U-CORP\swright <swright@V07C29D04123.corp.intuit.net>
2015-01-31 20:26:23 -0600

    getting response from server now, so far all it does is pop up in window

--- src/github.js
+++ src/github.js
@@ -2,33 +2,28 @@ window.onload = function() {
 }
 function loagGitPages(){
 var req = new XMLHttpRequest();
+
 if (!req)
 {
 	throw 'unable to create HttpRequest';
 }
 var citySelect = document.getElementsByName('');
 var url = 'https://api.github.com/gists';
-//https://api.github.com 
 //https://api.github.com/gists
-var params =  {q:"jackson,ms",mode:'json',units:'imperial', cnt: '7'};
+//var params =  {q:"jackson,ms",mode:'json',units:'imperial', cnt: '7'};
 //url += '?' + urlStringify(params);
-//var url = url + encodeURIComponent(params);
-//var xurl = encodeURIComponent(url);
-//req.onreadystatechange = function() {
- if (this.readyState === 4) {
-  if (req.status === 200) {
-		var weather = JSON.parse(this.Response.Text)
-		var maxt = list[0].temp.max;
-	};
+  req.onreadystatechange = function()
+  {
+  
+	if (req.readyState === 4) {
+		if (req.status === 200) {
+			  alert(req.responseText);
+		};
+	}
   }
   req.open('GET',url,true);
-  
-
   req.send();
   var x = 1;
-
-
-
 }
 
 
